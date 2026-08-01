daeb4f09ed5222a493bc13f6c463fc9dbe13d275
Teloxa <moralesdvd03@hotmail.com>
2026-07-31 23:32:41 -0600

    feat: implement initial book scraping functionality with axios and cheerio

--- src/scraper.js
+++ src/scraper.js
@@ -1,1 +1,32 @@
 // scrapping logic
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
+
+// set up
+const BASE_URL = 'https://books.toscrape.com/catalogue/page-';
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36';
+
+class bookScraper {
+constructor() {
+    this.books = [];
+    this.pagesToScrape = 5;
+    }
+
+    // Method to scrape book data from the website
+    async fetchPage(pageNumber) {
+        try {
+            const url = `${BASE_URL}${pageNumber}.html`;
+            console.log(` Page Scrapping ${pageNumber}...`);
+
+            const response = await axios.get(url{
+                headers: {
+                    'User-Agent': USER_AGENT
+                },
+                timeout: 10000
+            });
+            return response.data
+        } catch(error) {
+            console.log("Error case");
+        }
